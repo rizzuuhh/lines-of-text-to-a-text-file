dff19ba9ad0c643210f0148400f7e2c152fc04b2
rizza <denostarizza43@gmail.com>
2023-04-27 20:47:21 +1200

    Pseudocode

---
+++ lines.py
@@ -0,0 +1,17 @@
+# Import the required modules for the design of this program
+
+# print a welcome words for user
+
+# Open the mylife.txt file for writing
+
+# Ask for the for input until they say no
+  
+# Get a line of text from the user
+       
+# Write the entered line to the file
+        
+# Ask if the user wants to enter more lines
+       
+# Print to notify the user that the file has been saved
+
+
