43a0c65c0cd2506fbc3bc3f0eea00cf40f94cb9a
rizza <denostarizza43@gmail.com>
2023-04-27 21:04:48 +1200

    mylife.txt file opened

--- lines.py
+++ lines.py
@@ -6,8 +6,8 @@ import pyfiglet
 print(pyfiglet.figlet_format("HELLO, ENTER TEXT AS MUCH AS YOU WANT"))
 
 # Open the mylife.txt file for writing
-
-# Ask for the for input until they say no
+with open('mylife.txt', 'w') as f:
+    # Ask for the for input until they say no
   
 # Get a line of text from the user
        
@@ -16,5 +16,3 @@ print(pyfiglet.figlet_format("HELLO, ENTER TEXT AS MUCH AS YOU WANT"))
 # Ask if the user wants to enter more lines
        
 # Print to notify the user that the file has been saved
-
-
